7993fb835dd4c2ebead523bb714939782d13290e
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-07-31 22:01:25 +0530

    sign up

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,57 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const emailIsInvalid = inputs.email !== '' && !inputs.email.includes("@");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+    console.log(inputs);
+  };
+
+  const handleFieldsChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setInputs((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleFieldsChange}
+            value={inputs.email}
+          />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handleFieldsChange}
+            value={inputs.password}
+          />
         </div>
       </div>
 
